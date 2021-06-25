fccb16efc59fba14ef8910874d4d287972e4b278
kerathy <xxyxuxiaoyan@126.com>
2021-06-24 19:28:01 -0600

    card swipe view kotlin version

---
+++ card-swipe-view-kotlin-version/app/src/main/java/com/example/cardswipeviewkotlinversion/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.cardswipeviewkotlinversion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.viewpager.widget.ViewPager
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: PetAdapter
+    private lateinit var models: ArrayList<PetsModel>
+    private lateinit var viewPager: ViewPager
+    var sliderDotspanel: LinearLayout? = null
+    private var dotscount = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        viewPager = findViewById(R.id.view_pager)
+        sliderDotspanel = findViewById(R.id.slider_dots)
+
+        models = ArrayList()
+        models.add(PetsModel(R.drawable.dummy_image,"Dobby", "Dog"))
+        models.add(PetsModel(R.drawable.dummy_image,"Kitto", "Cat"))
+        models.add(PetsModel(R.drawable.dummy_image,"Cozmo", "Lambardor"))
+        models.add(PetsModel(R.drawable.dummy_image,"Tiger", "German Shepherd"))
+        models.add(PetsModel(R.drawable.dummy_image,"Husky", "Husky"))
+        models.add(PetsModel(R.drawable.dummy_image,"Cat", "Unknown"))
+        adapter = PetAdapter(models, this@MainActivity)
+        viewPager.adapter = adapter
+
+    }
+}
